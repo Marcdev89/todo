376772ae42930e7f58aeb667e71fa4049aa30f88
Marcdev89 <marc.vitoria89@gmail.com>
2023-05-21 13:21:06 +0200

    Added queries with filter match (title and username)

--- src/main/java/com/omc/todo/repository/TodoRepository.java
+++ src/main/java/com/omc/todo/repository/TodoRepository.java
@@ -1,8 +1,11 @@
 package com.omc.todo.repository;
 
 import com.omc.todo.entity.Todo;
-import com.omc.todo.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +14,11 @@ import java.util.List;
 public interface TodoRepository extends JpaRepository<Todo,Long> {
 
    List<Todo> findByUser(Long user_id);
+
+   @Query("SELECT t FROM Todo t WHERE t.title LIKE %?1%")
+   Page <Todo> findAll(String keyword, Pageable pageable);
+
+   @Query("SELECT t FROM Todo t WHERE (t.user.username = :username) AND (t.title LIKE %:title%)")
+   Page <Todo> findByUserNameAndTitle(@Param("username") String keywordUsername, @Param("title") String keywordTitle, Pageable pageable);
+
 }
