c8aa35f7e243bd82747be4f50a229334c38ed1a9
Marcdev89 <marc.vitoria89@gmail.com>
2023-05-21 13:30:02 +0200

    changed redirects, add new method to listByPage and delete method

--- src/main/java/com/omc/todo/controller/TodoThymeleafController.java
+++ src/main/java/com/omc/todo/controller/TodoThymeleafController.java
@@ -1,17 +1,15 @@
 package com.omc.todo.controller;
 
 import com.omc.todo.entity.Todo;
+import com.omc.todo.entity.User;
 import com.omc.todo.service.TodoService;
+import com.omc.todo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
@@ -19,28 +17,78 @@ import java.util.List;
 public class TodoThymeleafController {
     @Autowired
     private TodoService todoService;
-    @GetMapping({"/todo","/"})
-    public String todoList(Model model){
-        model.addAttribute("todo", todoService.getTodoList());
-        return "sorted";
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping({"/home","/"})
+    public String homePage(Model model) {
+        return listByPage(model, 1,"title","asc", "","");
     }
-    @GetMapping("/todo/pages/{pageNumber}/{field}/{order}")
-    public ModelAndView sortedList(@PathVariable("pageNumber") int pageNumber, @PathVariable("field") String field, @PathVariable("order") String order){
-        ModelAndView mav = new ModelAndView("sorted");
-        int size = 10;
-        Page<Todo> page = todoService.getTodoListPages(pageNumber,size, field,order);
+
+    @GetMapping("/todo/new")
+    public String formCreateTodo(Model model) {
+        model.addAttribute("users", userService.getUsers());
+        Todo todoPost = new Todo();
+        model.addAttribute("todoPost", todoPost);
+        return "newtodo";
+    }
+
+    @PostMapping("/save")
+    public String saveTodo(@ModelAttribute("todo") Todo todo){
+        User user = userService.getUserById(todo.getUser().getId());
+        todo.setUser(user);
+        todoService.save(todo);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String updateTodo(@PathVariable Long id, Model model) {
+        Todo modified = todoService.getTodoById(id);
+        List<Todo> todoList = todoService.getTodoList();
+        model.addAttribute("title","Form edit");
+        model.addAttribute("completed","Form edit");
+        model.addAttribute("modified", modified);
+        model.addAttribute("todoList", todoList);
+        return "edit";
+    }
+
+    @PostMapping("/edited")
+    public String editTodo(@ModelAttribute("todo") Todo todo){
+             todoService.updateTask(todo, todo.getId());
+        return "redirect:/home";
+    }
+    @GetMapping("/page/{pageNumber}")
+    public String listByPage(Model model,
+                             @PathVariable("pageNumber") int currentPage,
+                             @Param("sortField") String sortField,
+                             @Param("order") String order,
+                             @Param("keywordTitle") String keywordTitle,
+                             @Param("keywordUsername") String keywordUsername) {
+
+        Page<Todo> page = todoService.listAll(currentPage,sortField,order,keywordTitle, keywordUsername);
+
+        long totalItems = page.getTotalElements();
+        int totalPages = page.getTotalPages();
+
         List<Todo> todoList = page.getContent();
 
-        mav.addObject("pageNumber", pageNumber);
-        mav.addObject("totalPages",page.getTotalPages());
-        mav.addObject("Elements", page.getTotalElements());
-        mav.addObject("todoList", todoList);
-        mav.addObject("field", field);
-        mav.addObject("order", order.equals("asc") ? "asc" : "desc");
+        model.addAttribute("todoList", todoList);
+        model.addAttribute("currentPage",currentPage);
+        model.addAttribute("totalItems",totalItems);
+        model.addAttribute("totalPages",totalPages);
+        model.addAttribute("sortField",sortField);
+        model.addAttribute("order", order);
+        model.addAttribute("keywordTitle", keywordTitle);
+        model.addAttribute("keywordUsername", keywordUsername);
 
-
-
-        return mav;
-
+        String reverseOrder = order.equals("asc") ? "desc" : "asc";
+        model.addAttribute("reverseOrder", reverseOrder);
+        return "home";
     }
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id){
+        todoService.deleteById(id);
+    }
+
+
 }
